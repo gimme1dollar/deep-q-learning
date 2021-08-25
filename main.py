cf1ff1dff6d2f728e6262ec75f4b578df092e474
JYLee <gimme1dollar@postech.ac.kr>
2021-08-25 23:28:46 +0900

    change tarining scheme

--- main.py
+++ main.py
@@ -1,176 +1,120 @@
 import numpy as np
 import random
+import math
 import gym.spaces
 from itertools import count
+from collections import namedtuple
 from tqdm import tqdm
 from matplotlib import pyplot as plt
 
-from model.dqn import DQN
+from model.dqn import DQN, image_embedding
 from model.dqn import ReplayBuffer, select_epilson_greedy_action
-from util.schedule import LinearSchedule
-from util.gym import get_env, get_wrapper_by_name
-
+from util.gym import get_env
 import torch
 import torch.optim as optim
+import torch.nn.functional as F
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Hyperparameters
-env_name = "SpaceInvadersNoFrameskip-v0"
-env = get_env(env_name, 0)
-replay_buffer_size=1000000
-batch_size=32
-gamma=0.99
-learning_starts=50000
-learning_freq=4
-frame_history_len=4
-target_update_freq=10000
-expname=None
-total_timestep=1_000_000
-evaluation_timestep = 500
-exploration = LinearSchedule(total_timestep, 0.1)
-log_every_n_steps = 10000
-learning_rate=0.00025
-alpha=0.95
-eps=0.01
-device = "cuda" if torch.cuda.is_available() else "cpu"
+import warnings
+warnings.filterwarnings("ignore")
 
-# Build model
+# hyperparameters
+N_EPISODES = 400
+N_FRAMES = 4
+BATCH_SIZE = 32
+LEARNING_RATE = 0.00025
+TARGET_UPDATE = 1000
+GAMMA = 0.99 # Q-value
+MEMORY_SIZE = 100_000
+
+def get_epsilone(steps_done, EPS_START=1, EPS_END=1000000, EPS_DECAY=1000000):
+    return EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY), steps_done+1
+
+# make env
+env_name = "SpaceInvadersNoFrameskip-v0"
+env = gym.make(env_name)
+env = get_env(env, N_FRAMES)
+
+# build model
 img_h, img_w, img_c = env.observation_space.shape
-input_arg = frame_history_len * img_c
+in_channels = img_c
 num_actions = env.action_space.n
 
-# Initialize target q function and q function
-policy = DQN(input_arg, num_actions).to(device)
-target = DQN(input_arg, num_actions).to(device)
+policy = DQN(in_channels=in_channels, num_actions=num_actions).to(device)
+target = DQN(in_channels=in_channels, num_actions=num_actions).to(device)
+target.load_state_dict(policy.state_dict())
 
-# Construct Q network optimizer function
-criterion = torch.nn.MSELoss(reduction='none')
-optimizer = optim.RMSprop(policy.parameters(), lr=learning_rate, alpha=alpha, eps=eps)
+criterion = F.smooth_l1_loss
+optimizer = optim.Adam(policy.parameters(), lr=LEARNING_RATE)
 
-# Construct the replay buffer
-replay_buffer = ReplayBuffer(replay_buffer_size, frame_history_len)
-validation_buffer = ReplayBuffer(replay_buffer_size, frame_history_len)
+# replay buffer
+Transition = namedtuple('Transition', 
+                        ('state', 'action', 'next_state', 'reward'))
+memory = ReplayBuffer(MEMORY_SIZE)
 
-# Fill Validation Buffer
-done = True
-val_env = get_env(env_name, 0)
-for t in count():
-    if t > evaluation_timestep:
-        break
-    if done:
-        obs, done = val_env.reset(), False
-    last_idx = validation_buffer.store_frame(obs)
-    obs, _, done, _ = val_env.step(np.random.randint(0, num_actions))
-    validation_buffer.store_effect(last_idx, 0, 0, done)
-
-# Run env
-num_param_updates = 0
-mean_episode_reward = -float('nan')
-best_mean_episode_reward = -float('inf')
-
-# Logger
-eval_reward = -float('inf')
-eval_steps, eval_rewards, eval_q_values = [], [], []
-
-# Main loop
+# training
+episode_counts, episode_reward = [], []
+episode_count, steps_done = 0, 0
 last_obs = env.reset()
-with tqdm(total=total_timestep) as pbar:
+for episode in tqdm(range(N_EPISODES)):
+    obs = env.reset()
+    state = image_embedding(obs)
+    total_reward = 0.0
     for t in count():
-        pbar.n = t
-        pbar.refresh()
-
-        if t >= total_timestep:
-            break
-
-        last_idx, prev_obs = replay_buffer.store_frame(last_obs), replay_buffer.encode_recent_observation()
-        action = select_epilson_greedy_action(policy, prev_obs, exploration.value(t)) if t > learning_starts else random.randrange(num_actions)
-        curr_obs, reward, done, _ = env.step(action)
-        reward = max(-1.0, min(reward, 1.0)) # clip rewards between -1 and 1
-        replay_buffer.store_effect(last_idx, action, reward, done)
+        EPSILON, steps_done = get_epsilone(steps_done)
+        action = select_epilson_greedy_action(policy, state, EPSILON)
+        obs, reward, done, info = env.step(action)
 
         if done:
-            curr_obs = env.reset()
-        last_obs = curr_obs
+            next_state = None
+        else:
+            next_state = image_embedding(obs)
+        total_reward += reward
 
-        # Train network.
-        if (t > learning_starts and
-            t % learning_freq == 0 and
-            replay_buffer.can_sample(batch_size)):
+        reward = torch.tensor([reward], device=device)
+        memory.push(state, action.cpu(), next_state, reward.cpu())
+        state = next_state
 
-            curr_obs_batch, action_batch, reward_batch, next_obs_batch, not_done_mask = replay_buffer.sample(batch_size)
-            # done_mask[i] is 1 if the next state is the end of an episode, so no Q-value at the next state
+        # no warm start
+        if len(memory) < BATCH_SIZE:
+            continue
+        transitions = memory.sample(BATCH_SIZE)
+        batch = Transition(*zip(*transitions))
 
-            current_Q_values = policy(curr_obs_batch).gather(1, action_batch.unsqueeze(1)).squeeze(1)
-            next_Q_values = target(next_obs_batch).detach().max(1)[0] * not_done_mask
-            target_Q_values = reward_batch + (gamma * next_Q_values)
-            bellman_error = criterion(current_Q_values, target_Q_values)
+        states   = torch.cat(batch.state).to(device)
+        actions  = torch.cat(tuple((map(lambda a: torch.tensor([[a]], device=device), batch.action))))
+        rewards  = torch.cat(tuple((map(lambda r: torch.tensor([r], device=device), batch.reward))))
+        dones    = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=device, dtype=torch.uint8)
+        n_states = torch.cat([s for s in batch.next_state if s is not None]).to(device)
 
-            optimizer.zero_grad()
-            bellman_error = torch.clamp(bellman_error, 0, 1).sum()
-            bellman_error.backward()
-            num_param_updates += 1
-            optimizer.step()
+        # compute Q-values
+        cur_Q = policy(states).gather(1, actions)
+        nxt_Q = torch.zeros(BATCH_SIZE, device=device)
+        nxt_Q[dones] = target(n_states).max(1)[0].detach()
+        exp_Q = (nxt_Q * GAMMA) + rewards
+        loss = F.smooth_l1_loss(cur_Q, nxt_Q.unsqueeze(1))
+        
+        # step optimizer
+        optimizer.zero_grad()
+        loss.backward()
+        for param in policy.parameters():
+            param.grad.data.clamp_(-1, 1)
+        optimizer.step()
 
-            if num_param_updates % target_update_freq == 0:
-                target.load_state_dict(policy.state_dict())
+        if steps_done % TARGET_UPDATE == 0:
+            target.load_state_dict(policy.state_dict())
 
-        # Logger
-        episode_rewards = get_wrapper_by_name(env, "Monitor").get_episode_rewards()
-        if len(episode_rewards) > 100:
-            mean_episode_reward = np.mean(episode_rewards[-100:])
-            best_mean_episode_reward = max(best_mean_episode_reward, mean_episode_reward)
+        if done:
+            break
+    episode_count += 1
+    episode_counts.append(episode_count)
+    episode_reward.append(total_reward)
+env.close()
 
-        # Evaluation
-        if t % log_every_n_steps == 0 and t > learning_starts:
-            policy.eval()
-
-            tqdm.write("===============Training==================")
-            tqdm.write(f"timestep {t} & episodes {len(episode_rewards)}")
-            tqdm.write(f"mean reward (100 episodes) {mean_episode_reward}")
-            tqdm.write(f"best mean reward {best_mean_episode_reward}")
-            #tqdm.write(f"exploration {exploration.value(t)}")
-
-            # Test Q-values over validation memory
-            qs = []
-            with torch.no_grad():
-                for state in validation_buffer.get_all_frames():  # Iterate over valid states
-                    state = (torch.from_numpy(state).type(torch.cuda.FloatTensor) / 255.0)[None, ...]
-                    qs.append(policy(state).max(1)[0].item())
-            eval_reward = max(np.mean(episode_rewards[-5:]), eval_reward)
-            eval_steps.append(t)
-            eval_rewards.append(eval_reward)
-            eval_q_values.append(np.mean(qs))
-            tqdm.write("===============Validation=================")
-            tqdm.write(f"Rewards {eval_rewards[-1]}")
-            tqdm.write(f"steps {eval_steps[-1]}")
-            tqdm.write(f"mean Q-values {eval_q_values[-1]}")
-            tqdm.write("\n")
-
-            policy.train()
-
-torch.save(policy.state_dict(), f'./result/policy_{t:06}.pt')
+# save figure
+torch.save(policy.state_dict(), f'./result/policy.pt')
 plt.figure(figsize=(15, 15))
-plt.subplot(2, 1, 1)
 plt.title('reward')
-plt.plot(eval_steps, eval_rewards, 'r')
-plt.subplot(2, 1, 2)
-plt.title('q values')
-plt.plot(eval_steps, eval_q_values, 'b')
-plt.tick_params(axis='both', direction='in', length=3, pad=6, labelsize=14)
+plt.plot(episode_counts, episode_reward, 'r')
 plt.savefig('./result/metrics.png')
 
-'''
-policy.eval()
-vid_env = get_env(env_name, 0, (0,))
-vid_env.reset()
-done = False
-tmp_buf = ReplayBuffer(10000, frame_history_len)
-for t in count():
-    if done:
-        break
-    last_idx = tmp_buf.store_frame(obs)
-    recent_observations = replay_buffer.encode_recent_observation()
-    action = select_epilson_greedy_action(policy, recent_observations, 0.0)
-    obs, _, done, _ = vid_env.step(action)
-    tmp_buf.store_effect(last_idx, 0, 0, done)
-'''
